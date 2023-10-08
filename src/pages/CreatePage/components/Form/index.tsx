01b7a7fd23bbd7501e626ffae409817dcde9cf6e
Hylan Silva <hylansilva@outlook.com>
2023-10-08 10:45:11 -0300

    removendo o chakra-ui

--- src/pages/CreatePage/components/Form/index.tsx
+++ src/pages/CreatePage/components/Form/index.tsx
@@ -1,23 +1,6 @@
-import { useForm } from "react-hook-form"
-
-type formData = {
-    assistaint: string,
-    typeHistory: string,
-    typeChar: string,
-    typeMission: string,
-    age: number
-}
-
-
-
-export default function Form(){
-    const {
-        register,
-        setValue,
-        handleSubmit,
-        formstate: { errors },
-    } = useForm<FormData>()
-    return (
-        <form
-    )
+export default function FormPrompt(){
+  return(
+    <form>
+    </form>
+  )
 }
